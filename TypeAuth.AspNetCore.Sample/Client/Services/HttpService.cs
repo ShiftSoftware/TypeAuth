2202e199104cd1f465bb9c2e1a6157dafcbd1274
Nahro <nahropro@gmail.com>
2022-10-30 18:01:05 +0300

    Create HttpService for client sample

---
+++ TypeAuth.AspNetCore.Sample/Client/Services/HttpService.cs
@@ -0,0 +1,53 @@
+﻿using System.Net.Http.Json;
+using System.Net;
+using TypeAuth.AspNetCore.Sample.Client.Extensions;
+using TypeAuth.AspNetCore.Sample.Client.Services.Interfaces;
+
+namespace TypeAuth.AspNetCore.Sample.Client.Services
+{
+    public class HttpService : IHttpService
+    {
+        private readonly HttpClient http;
+
+        public HttpService(HttpClient http)
+        {
+            this.http = http;
+        }
+
+        public async Task<TResult> PostAsync<TResult, TValue>(string url, TValue value)
+        {
+            using var response = await http.PostAsJsonAsync(url, value);
+
+            if (response.StatusCode != HttpStatusCode.OK)
+                return default;
+
+            return await response.Content.ReadFromJsonAsync<TResult>();
+        }
+
+        public async Task<TResult> GetAsync<TResult>(string url, object query = null)
+        {
+            var queryString = query?.ToQueryString() ?? null;
+            return await http.GetFromJsonAsync<TResult>(url + (queryString is not null ? "?" + queryString : ""));
+        }
+
+        public async Task<TResult> PutAsync<TResult, TValue>(string url, TValue value)
+        {
+            using var response = await http.PutAsJsonAsync(url, value);
+
+            if (response.StatusCode != HttpStatusCode.OK)
+                return default;
+
+            return await response.Content.ReadFromJsonAsync<TResult>();
+        }
+
+        public async Task<bool> DeleteAsync(string url)
+        {
+            using var response = await http.DeleteAsync(url);
+
+            if (response.StatusCode != HttpStatusCode.OK)
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ TypeAuth.AspNetCore.Sample/Client/Services/Interfaces/IHttpService.cs
@@ -0,0 +1,10 @@
+﻿namespace TypeAuth.AspNetCore.Sample.Client.Services.Interfaces
+{
+    public interface IHttpService
+    {
+        Task<bool> DeleteAsync(string url);
+        Task<TResult> GetAsync<TResult>(string url, object query = null);
+        Task<TResult> PostAsync<TResult, TValue>(string url, TValue value);
+        Task<TResult> PutAsync<TResult, TValue>(string url, TValue value);
+    }
+}
