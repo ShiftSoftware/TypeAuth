c3c22390976b146739aff9bd0d9359c3c801b547
Nahro <nahropro@gmail.com>
2022-10-30 21:33:22 +0300

    Create ActionTreeModel for sample

---
+++ TypeAuth.AspNetCore.Sample/Server/ActionTreeModels/CRMActionModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeAuth.AspNetCore.Sample.Server.ActionTreeModels
+{
+    public class CRMActionModel
+    {
+        public int[] Sales { get; set; }
+
+        public string SalesDiscountValue { get; set; }
+    }
+}
