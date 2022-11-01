671f65d971b84f276d82aaf4e90dc999d8dc1bfb
Nahro <nahropro@gmail.com>
2022-11-01 23:52:38 +0300

    Crate TypeAuthClaimTypes for type-auth specefic claim-types

---
+++ TypeAuth.AspNetCore/TypeAuthClaimTypes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeAuth.AspNetCore
+{
+    /// <summary>
+    /// Contains TypeAuth spesific claim types
+    /// </summary>
+    public class TypeAuthClaimTypes
+    {
+        /// <summary>
+        /// Default TypeAuth calim type for access tree, Shift/Software/TypeAuth/Claims/AccessTree.
+        /// </summary>
+        public const string AccessTree = "Shift/Software/TypeAuth/Claims/AccessTree";
+    }
+}
