04d559490529c929d67566331b9c86ea8deeba8c
Aza Asim <azaasim@gmail.com>
2023-01-23 15:49:48 +0300

    Use Dynamic for AdditionalData in ActionTreeItem

--- TypeAuth.Core/ActionTreeItem.cs
+++ TypeAuth.Core/ActionTreeItem.cs
@@ -5,7 +5,7 @@ public class ActionTreeItem
     public string TypeName { get; set; } = default!;
     public string? DisplayName { get; set; }
     public string? DisplayDescription { get; set; }
-    public Dictionary<string, object> AdditionalData { get; set; } = new Dictionary<string, object>();
+    public dynamic? AdditionalData { get; set; }
     public Actions.Action? Action { get; set; }
     public HashSet<ActionTreeItem> ActionTreeItems { get; set; }
 
